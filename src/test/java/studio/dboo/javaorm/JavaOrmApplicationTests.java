f82f422374171c32782c8a589fbca045f13b9138
dboo <dboo.studio@gmail.com>
2021-10-21 17:53:49 +0900

    first commit

---
+++ src/test/java/studio/dboo/javaorm/JavaOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.dboo.javaorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
